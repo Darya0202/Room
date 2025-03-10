78804690342f423321254e9e78df9b42e52ef29d
Hummingbird <mosb-202_768966@volsu.ru>
2025-03-10 15:45:01 +0300

    Lesson 5

--- app/src/main/java/com/example/room/screens/start/StartFragment.kt
+++ app/src/main/java/com/example/room/screens/start/StartFragment.kt
@@ -5,16 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.example.room.APP
 import com.example.room.R
+import com.example.room.adapter.NoteAdapter
+import com.example.room.databinding.FragmentStartBinding
 
 class StartFragment : Fragment() {
+    lateinit var binding: FragmentStartBinding
+    lateinit var recyclerView: RecyclerView
+    lateinit var adapter: NoteAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+        binding = FragmentStartBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        val viewModel = ViewModelProvider(this).get(StartViewModel::class.java)
+        viewModel.initDataBase()
+        recyclerView = binding.rvNotes
+        adapter = NoteAdapter()
+        recyclerView.adapter = adapter
+        viewModel.getAllNotes().observe(viewLifecycleOwner,{ListNotes ->
+            ListNotes.asReversed()
+            adapter.setList(ListNotes)
+        })
+
+        binding.btnNext.setOnClickListener{
+            APP.navController.navigate(R.id.action_startFragment_to_addNoteFragment)
+        }
     }
 
 }
